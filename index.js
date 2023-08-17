a26b8f42c99778a5a40e7abf635ea925920a8a37
CryptoKeyPlayer <62218123+SCIFER99@users.noreply.github.com>
2023-08-16 21:41:47 -0400

    Uploaded Files!

---
+++ index.js
@@ -0,0 +1,11 @@
+// page/index.js
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to My Next.js App!</h1>
+        </div>
+    );
+};
+
+export default HomePage;
+
---
+++ next.config.js
@@ -0,0 +1,5 @@
+// next.config.js
+module.exports = {
+    // Your Next.js configuration options go here!
+};
+
